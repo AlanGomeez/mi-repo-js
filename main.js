b94ce9b44b1a33e8d43fdacfcf73362fad8caec1
Alan Gomez <toty1999gomez@gmail.com>
2023-08-16 12:03:04 -0300

    preentrega2 modificaciones

--- main.js
+++ main.js
@@ -14,41 +14,41 @@ if(turno === "3107"){
     alert("Fecha de turno incorrecta. Vuelva a ingresar su turno nuevamente.");
 }
 
-// let reprogramar = prompt("¿Quiere reprogramar su turno? (responda Si o No)");
-// switch (reprogramar.toLowerCase()) {
-//     case "no":
-//         alert("Perfecto, Gracias por confirmar.");
-//         break;
-//     case "si":
-//         let nuevaFecha = prompt("Ingrese la nueva fecha de turno (en formato DDMM):");
-//         alert("Su turno ha sido reprogramado para el " + nuevaFecha + ", Muchas gracias.");
-//         break;   
-//     default:
-//         alert("Repuesta inválida. Por favor, responda Si o No.");
-//         break;
-// }
+let reprogramar = prompt("¿Quiere reprogramar su turno? (responda Si o No)");
+switch (reprogramar.toLowerCase()) {
+    case "no":
+        alert("Perfecto, Gracias por confirmar.");
+        break;
+    case "si":
+        let nuevaFecha = prompt("Ingrese la nueva fecha de turno (en formato DDMM):");
+        alert("Su turno ha sido reprogramado para el " + nuevaFecha + ", Muchas gracias.");
+        break;   
+    default:
+        alert("Repuesta inválida. Por favor, responda Si o No.");
+        break;
+}
 
-// let producto = prompt("¿Desea comprar algun producto?");
-// switch (producto.toLowerCase()) {
-//     case "no":
-//         alert("De acuerdo, muchas gracias.")
-//         break;
-//     case "si":
-//         alert("De acuerdo, estos son los siguientes productos:\n 1. omeprazol 250mg\n 2. tafirol 250mg\n 3. actron 600mg.");
-//         break;
-//     default:
-//         alert("No se agrego ningun producto al carrito.")
-//         break;
-// }
+let producto = prompt("¿Desea comprar algun producto?");
+switch (producto.toLowerCase()) {
+    case "no":
+        alert("De acuerdo, muchas gracias.")
+        break;
+    case "si":
+        alert("De acuerdo, estos son los siguientes productos:\n 1. omeprazol 250mg\n 2. tafirol 250mg\n 3. actron 600mg.");
+        break;
+    default:
+        alert("No se agrego ningun producto al carrito.")
+        break;
+}
 
-// let compra = prompt("Presione cual opción desea.");
-// switch (compra.toLowerCase()) {
-//     case "1": 
-//     alert("De acuerdo, agregaremos al carrito el producto: omeprazol."); break;
-//     case "2": alert("De acuerdo, agregaremos al carrito el producto: tafirol."); break;
-//     case "3": alert("De acuerdo, agregaremos al carrito el producto: actron 600mg."); 
-//     default: alert("Opción invalida."); break;
-// }
+let compra = prompt("Presione cual opción desea.");
+switch (compra.toLowerCase()) {
+    case "1": 
+    alert("De acuerdo, agregaremos al carrito el producto: omeprazol."); break;
+    case "2": alert("De acuerdo, agregaremos al carrito el producto: tafirol."); break;
+    case "3": alert("De acuerdo, agregaremos al carrito el producto: actron 600mg."); 
+    default: alert("Opción invalida."); break;
+}
 
 
 const productos = [
@@ -72,22 +72,11 @@ filtrados.forEach((item) => {
 
 
 
-let resumen = prompt("Eso seria todo: " + turno + filtrados);
 
 
 
 
-
-// let resumen = prompt("En resumen: ");
-// let resumen = resumen.find((item) => item.nombre === nombre);
-
-// if (nombre) {
-//    alert(`
-//     id: ${item.id}
-//     nombre: ${item.nombre}
-//     precio: $${item.precio}
-//     mediosDePago: ${item.mediosDePago}
-//     `);
-// } else {
-//     alert(`Producto no encontrado.`)
-// }
+// let resumen = productos.map((item) => {
+//     return item.id, item.nombre, item.precio, item.mediosDePago
+// });
+// alert("Resumen de todo " + resumen + turno);
